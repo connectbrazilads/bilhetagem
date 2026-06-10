e0b88f60730f75004d654736c511dab24d4266e4
connectbrazilads <connectbrazilads@gmail.com>
2026-06-10 14:03:52 -0300

    Restore frontend lint command

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,24 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+
+const config = [
+  {
+    ignores: [
+      ".next/**",
+      "node_modules/**",
+      "next-env.d.ts",
+      "tsconfig.tsbuildinfo",
+    ],
+  },
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-require-imports": "off",
+      "react-hooks/set-state-in-effect": "off",
+    },
+  },
+];
+
+export default config;
